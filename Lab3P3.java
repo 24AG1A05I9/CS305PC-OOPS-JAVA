42a80ea479cea6c4a2f5624ac3b5047bcf582ad2
24AG1A05I9 <akashvadithya19@gmail.com>
2025-09-17 12:53:03 +0530

    Add files via upload

---
+++ Lab3P3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Lab3P3{
+	public static void main(String... args){
+		Lab3P3 l = new Lab3P3();
+		l.display();
+		l.dispaly(10);
+		//l.display("string");
+	}
+	public void show(){
+		System.out.println("without para");
+	}
+	public void show(int x){
+		System.out.println("the parameter " + x);
+	}
+	
+	
+}
