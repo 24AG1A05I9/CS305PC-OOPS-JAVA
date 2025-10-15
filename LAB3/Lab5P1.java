5ca3db305917c43b56213042cbfe0f1b177ead91
24AG1A05I9 <akashvadithya19@gmail.com>
2025-10-15 10:30:56 +0530

    Add files via upload

---
+++ LAB3/Lab5P1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try{
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(x/y);
+        }
+        catch(InputMismatchException ie){
+            System.out.println(ie.getClass().getName());
+            
+            
+        }
+        catch(ArithmeticException ie){
+            System.out.println(ie.getClass().getName()+ ": " + ie.getMessage());
+        }
+
+    }
+}
+
